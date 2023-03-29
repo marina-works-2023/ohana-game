0fec4dd32aeb3904a08184d93899100bd218608e
marina-works-2023 <125856313+marina-works-2023@users.noreply.github.com>
2023-03-29 19:09:23 +0900

    アラートまで0.5秒止める
    アラートまで0.5秒止まらないバグを修正

--- ohana_game.js
+++ ohana_game.js
@@ -131,8 +131,8 @@ function update(player, header, image, buttonWater, buttonSun, buttonHiryo) {
 //終了処理をする関数
 function end(buttonWater,buttonSun, buttonHiryo,result) {
     buttonOff(buttonWater, buttonSun, buttonHiryo); //ボタン非活性関数の実行
-    setTimeout(alert(result), 500); //アラートまで0.5秒止める
-    
+    setTimeout(function(){ alert(result) }, 500); //アラートまで0.5秒止める
+
     //ヘッダー
     const endHeader = document.createElement('h4');
     endHeader.innerText = '▼▼▼お花の育て方の参考にしてね▼▼▼';
